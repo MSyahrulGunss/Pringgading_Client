1f2fcd461ac6fd10a9341c6d64a940723bc65b36
MSyahrulGunss <syahrulhahaha39@gmail.com>
2024-07-31 20:33:00 +0700

    first

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -3,49 +3,7 @@ class Footer extends HTMLElement {
         this.innerHTML = `
             <footer class="text-white pt-20 pb-19 ">
                 <div class=" text-center text-md-left">
-                    <div class="row text-center text-md-left">
-                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Tentang Website</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi accusamus quae vitae possimus expedita laboriosam hic ex at ratione consectetur odio amet exercitationem mollitia, neque nihil cum cupiditate, sit ipsam!</p>
-                        </div>
-                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-2">
-                            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Kegiatan</h5>
-                            <p>
-                                <a href="#" class="text-white" style="text-decoration: none;">Kegiatan harian</a>
-                            </p>
-                            <p>
-                                <a href="#" class="text-white" style="text-decoration: none;">Kegiatan mingguan</a>
-                            </p>
-                            <p>
-                                <a href="#" class="text-white" style="text-decoration: none;">Kegiatan bulanan</a>
-                            </p>
-                        </div>
-
-                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Program</h5>
-                            <p>
-                                <a href="#" class="text-white" style="text-decoration: none;">Pengolahan Sampah</a>
-                            </p>
-                            <p>
-                                <a href="#" class="text-white" style="text-decoration: none;">Integrated Farming</a>
-                            </p>
-                        </div>
-                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                            <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact</h5>
-                            <p>
-                                <i class="fas fa-home mr-3"></i> Guwosari, Bantul
-                            </p>
-                            <p>
-                                <i class="fas fa-envelope mr-3"></i> email@gmail
-                            </p>
-                            <p>
-                                <i class="fas fa-phone mr-3"></i> +62 
-                            </p>
-                        </div>
-                    </div>
                     <hr class="mb-4">
-
-              
                     <div class="row justify-content-center">
                         <div class="col-md-7 col-lg-8">
                             <p>Sosial
